b5493aa626ddb4e3de66ba019d5445f441250958
George Leinov <george@Georges-MacBook-Pro.local>
2023-03-27 23:16:40 +0300

    commit daily challange

---
+++ Java_script/week_3/day_2/Daily_Challange/script.js
@@ -0,0 +1,19 @@
+// Exercise 1
+    const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+    // console.log(fruits) //test purpose
+    fruits.shift()
+    // console.log(fruits) //test purpose
+    fruits.sort()
+    // console.log(fruits) //test purpose
+    fruits.push("Kiwi")
+    // console.log(fruits) //test purpose
+    fruits.splice(0,1)
+    // console.log(fruits) //test purpose
+    fruits.reverse()
+    console.log(fruits)
+// Exercise 1           END
+
+// Exercise 2
+    const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+    console.log((moreFruits[1][1][0]))
+
