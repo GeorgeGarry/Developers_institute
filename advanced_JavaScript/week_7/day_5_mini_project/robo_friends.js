4be0cbd32b98ca5516906cb04e11e54ff0f0ba73
George Leinov <george@Georges-MacBook-Pro.local>
2023-05-13 21:15:40 +0300

    brushing up the code and adding comments

--- advanced_JavaScript/week_7/day_5_mini_project/robo_friends.js
+++ advanced_JavaScript/week_7/day_5_mini_project/robo_friends.js
@@ -70,40 +70,56 @@ const robots = [
       image:'https://robohash.org/10?200x200'
     }
     ];
-let my_divs = []
+
+let my_divs = [];
+
 for (i of robots){
-    let robots_div = document.getElementsByClassName("container")[0];
-    let new_robot_div = document.createElement("div");  // div for each robot
-    let robot_name = document.createElement("h3");
+    let robots_container = document.getElementsByClassName("container")[0]; //container for robots
+        //one single tobot div
+    let new_robot_div = document.createElement("div");
+    new_robot_div.classList.add("robots");
+    new_robot_div.setAttribute("name", "");
+    new_robot_div.name = i.name ;
+    //one single tobot div
+
+        //robot's name
+    let robot_name = document.createElement("h3");  
     robot_name.innerText = i.name;
-    robot_name.classList.add("inside_text")
+    robot_name.classList.add("inside_text");
+    //robot's name
+
+        //robot's email
     let robot_email = document.createElement("p");
-    robot_email.innerText = i.email
-    robot_email.classList.add("inside_text")
+    robot_email.innerText = i.email;
+    robot_email.classList.add("inside_text");
+    //robot's email
+
+        //robot's picture
     let robot_img = document.createElement("img");
-    
     robot_img.src = i.image;
     robot_img.classList.add("images");
-        // image
+    //robot's picture
+
+    //adding inner elements to one robot div
     new_robot_div.appendChild(robot_img);
     new_robot_div.appendChild(robot_name);
     new_robot_div.appendChild(robot_email);
-    new_robot_div.classList.add("robots");
-    new_robot_div.id = i.username
-    new_robot_div.setAttribute("name", "")
-    new_robot_div.name = i.name
-    let divs_key = i.username
-    robots_div.appendChild(new_robot_div);
-    my_divs.push(new_robot_div)
+        //adding inner elements to one robot div
     
-}
+    robots_container.appendChild(new_robot_div);  //append new robot to the container
+    
+    my_divs.push(new_robot_div);    //array of robots' divs to use in the search function
+    
+};
 
-console.log(my_divs[0].name);
 function search(e){
     let my_search = e.target.value.toUpperCase();
-    // my_search = my_search.toUpperCase()
-    let robots_div = document.getElementsByClassName("container")[0];
-    document.body.removeChild(robots_div);
+    //clearing the page for new search
+    let robots_container = document.getElementsByClassName("container")[0];
+    document.body.removeChild(robots_container);
+        //clearing the page for new search
+
+    //new container to display only searched robots
     let new_div_search = document.createElement("div");
     new_div_search.classList.add("container");
     for (i of my_divs){
@@ -112,5 +128,5 @@ function search(e){
         }
     }
     document.body.appendChild(new_div_search);
-}
-
+        //new container to display only searched robots
+};
